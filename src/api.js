dfda2fd2e51857862ca6b7ecd2316e029048ac8a
Alice Abarca <aliceabarca@otmail.com>
2023-09-11 10:10:05 -0600

    Add get fetch call

---
+++ src/api.js
@@ -0,0 +1,6 @@
+export function getFetch() {
+  fetch('http://localhost:3001/api/v1/reservations')
+  .then(response => response.json)
+  .then(data => console.log(data))
+  .catch(err => err.message('Could Not Found Reservations'))
+}
