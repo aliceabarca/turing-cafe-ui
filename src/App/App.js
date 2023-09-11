aaead456d61fb3069ffba181e530b0c64ae35e54
Alice Abarca <aliceabarca@otmail.com>
2023-09-11 10:04:45 -0600

    Add App

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,33 @@
+import Form from '../components/Form/Form';
+import Reservation from '../components/Reservation/Reservation';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+
+  const data = [
+    {
+      id: 1,
+      name: "Christie",
+      date: "12/29",
+      time: "7:00",
+      number: 12
+      }
+  ]
+  const [reservation, setReservation] = useState([])
+
+  function customerAddRes(newRes) {
+    setReservation([...reservation, newRes])
+  }
+  
   return (
     <div className="App">
       <h1 className='app-title'>Turing Cafe Reservations</h1>
       <div className='resy-form'>
+        <Form customerAddRes={customerAddRes}/>
       </div>
       <div className='resy-container'>
+        <Reservation reservation={reservation}/>
       </div>
     </div>
   );
